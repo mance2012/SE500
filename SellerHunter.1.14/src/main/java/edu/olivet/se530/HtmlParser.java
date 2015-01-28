99d3abe1c59b475682d7da209e3b7d34eb988ea5
sarah ma <mance2012@gmail.com>
2015-01-27 20:40:28 -0800

    进行HtmlParserTest单元测试

--- SellerHunter.1.14/src/main/java/edu/olivet/se530/HtmlParser.java
+++ SellerHunter.1.14/src/main/java/edu/olivet/se530/HtmlParser.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import edu.olivet.se530.model.Condition;
 import edu.olivet.se530.model.Offer;
 import edu.olivet.se530.model.Seller;
 
@@ -16,16 +18,39 @@ public class HtmlParser {
 		Elements rows = doc.select("div.a-row.a-spacing-mini.olpOffer");
 		for (int i = 0; i < rows.size(); i++) {
 			Offer offer = new Offer();
+			Element row = rows.get(i);
+			
 			Seller seller = new Seller();
-			seller.setName(this.getText(doc, ""));
+			String SellerNameSellector = "p.a-spacing-samll.olpSellerName";
+			seller.setName(this.getText(row, SellerNameSellector));
+			Elements link = rows.select(SellerNameSellector + "a");
+			if( link.size() > 0) {
+				seller.setUuid(link.get(0).attr("href").replaceFirst(".*&seller=", ""));
+			}
+			
+			
+			offer.setPrice(Float.parseFloat(this.getText(row, "span.olpOfferPrice").replace("$", "")));
+			String shippingFeeText = this.getText(row, "span.olpShippingPrice").replace("$", "");
+			//需要再次进行处理
+			if(shippingFeeText != null && shippingFeeText.trim().length() > 0) {
+				offer.setPrice(Float.parseFloat(shippingFeeText));
+			}
+
+			String cond = this.getText(row, "h3.a-spacing-small olpCondition");
+			String[] array = cond.split("-");
+			Condition condition = new Condition();
+			condition.setPrimary(array[0].trim());
+			//condition.setSecondary(array[1].trim());
+			
 			offer.setSeller(seller);
+			offer.setCondition(condition);
 			results.add(offer);
 		}
 		return results;
 	}
 
-	public String getText(Document document, String selector) {
-		Elements elements = document.select(selector);
+	public String getText(Element element, String selector) {
+		Elements elements = element.select(selector);
 		if(elements.size() <= 0){
 			return "";
 		}
--- SellerHunter.1.14/src/test/java/edu/olivet/se530/HtmlParserTest.java
+++ SellerHunter.1.14/src/test/java/edu/olivet/se530/HtmlParserTest.java
@@ -1,15 +1,16 @@
 package edu.olivet.se530;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
 
 import org.junit.Assert;
 import org.jsoup.nodes.Document;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import edu.olivet.se530.HtmlParser;
 import edu.olivet.se530.dummy.DummyHtmlCrawler;
+import edu.olivet.se530.model.Offer;
 
 
 public class HtmlParserTest {
@@ -21,10 +22,19 @@ public class HtmlParserTest {
 	}
 	
 	@Test
+	public void testParseOffer() {
+		List<Offer> offers = htmlParser.parserOffer(document);
+		Assert.assertTrue(6.99f == offers.get(0).getPrice());
+		Assert.assertEquals("", offers.get(1).getCondition().getPrimary());
+		//Assert.assertEquals("", offers.get(1).getCondition().getSecondary());
+		
+	}
+	
+	@Test
 	public void testGetText() {
-		Assert.assertEquals("internationalbooks", htmlParser.getText(document, "#olpTabContent > div > div.a-section.a-spacing-double-large > div:nth-child(7) > div.a-column.a-span2.olpSellerColumn > p.a-spacing-small.olpSellerName > span > a"));
+		Assert.assertEquals("indoobestsellers", htmlParser.getText(document, "#olpTabContent > div > div.a-section.a-spacing-double-large > div:nth-child(7) > div.a-column.a-span2.olpSellerColumn > p.a-spacing-small.olpSellerName > span > a"));
 		Assert.assertEquals("In Stock.", htmlParser.getText(document, "#olpTabContent > div > div.a-section.a-spacing-double-large > div:nth-child(7) > div.a-column.a-span3.olpDeliveryColumn > ul > li:nth-child(1) > span"));
-		Assert.assertEquals("Amazon Prime TM", htmlParser.getText(document, "#olpTabContent > div > div.a-section.a-spacing-double-large > div:nth-child(7) > div:nth-child(1) > span.supersaver > i"));
+		Assert.assertEquals("Amazon Prime TM", htmlParser.getText(document, "#olpTabContent > div > div.a-section.a-spacing-double-large > div:nth-child(3) > div:nth-child(1) > span.supersaver > i > span"));
 	}
 
 }
